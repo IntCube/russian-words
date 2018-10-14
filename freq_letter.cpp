bfe00cce260bf6dcb2d68ac447bd92ea4957c112
IntCube <is.konstantin@gmail.com>
2018-10-14 13:05:01 +0800

    Update freq_letter.cpp

--- freq_letter.cpp
+++ freq_letter.cpp
@@ -1,65 +1,61 @@
-#include <iostream>        // поточный I/O
-#include <cstdlib>         // системные вызовы
-#include <fstream>         // файловый поток
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
 #include <conio.h>
 
-#define COUNT_LETTER 31          // количество букв алфавита 0-31 
-#define ANSI_OFFSET 32           // смещение для кодировки 1251
-#define VOCABULARY "russian.txt" // Русский словарь 
-                        
-// используем пространтсво имен
+#define COUNT_LETTER 31
+#define ANSI_OFFSET 32
+#define VOCABULARY "russian.txt"
+                       
 using namespace std;
 using std::cout;
 using std::endl;
 using std::ifstream;
 
 int main(){
-	setlocale(LC_ALL, "Russian"); // для вывода русских символов
+    setlocale(LC_ALL, "Russian");
 	
-	int letter[COUNT_LETTER]; // массив частотного распределения букв
-    int l_word[50]; // массив длин слов	 
-	int count_word=0; // количество слов
-	int count_char=0; // количество символов
-	char ch; // текущий считываемый символ
-	int len_word=0; // длина текущего слова
+    int letter[COUNT_LETTER];
+    int l_word[50]; 
+    int count_word=0;
+    int count_char=0;
+    char ch;
+    int len_word=0;
 	
-	ifstream fin(VOCABULARY);
-    cout << "Файл словаря: " << VOCABULARY << endl;
-    cout << "Идет подсчет..." << endl << endl;
-
-    // обнулять массивы, так как накопительные
+    ifstream fin(VOCABULARY);
+    cout << "РЎР»РѕРІР°СЂСЊ: " << VOCABULARY << endl;
+    cout << "РРґРµС‚ РїРѕРґСЃС‡РµС‚..." << endl << endl;
+	
     for(int i=0;i<32;i++){letter[i]=0;}
     for(i=0;i<51;i++){l_word[i]=0;}
     
-    // считывание посимвольно, пока не будет false
-	while (fin.get(ch)){
-		if (!(ch == '\n')){ // если знак не перевод строки
-			if ((ch >= 'А' && ch <= 'Я') || (ch >= 'а' && ch <= 'я')){
-				if (ch=='ё'){ch='е';} // Е и Ё - как одна буква
-				count_char++; // счетчик букв
-				tolower(ch); // букву в нижний регистр
-				len_word++;
-				letter[(int(ch)+ANSI_OFFSET)]++;} // индекс массива как смещение по кодировке 1251
-			else{}}
-		else{
-			count_word++; // счетчик слов
-			l_word[len_word]++; len_word=0;}} // накопительные массивы по количеству букв 
+    while (fin.get(ch)){
+	if (!(ch == '\n')){
+	    if ((ch >= 'Рђ' && ch <= 'РЇ') || (ch >= 'Р°' && ch <= 'СЏ')){
+		if (ch=='С‘'){ch='Рµ';} // as one letter
+		count_char++;
+		tolower(ch);
+		len_word++;
+		letter[(int(ch)+ANSI_OFFSET)]++;} // index array as offset encoding Windows1251 to 0
+	    else{}}
+	else{
+	    count_word++;
+	    l_word[len_word]++; len_word=0;}}
     fin.close();
 
-    // Формирование потока вывода
-    cout << "Частотный анализ букв" << endl << "----------------" << endl;
-	for(char c='а'; c<='я'; c++){
-        cout << "Буква " << c << " " << letter[(int(c)+ANSI_OFFSET)] << endl;}
+    cout << "Р§Р°СЃС‚РѕС‚РЅС‹Р№ Р°РЅР°Р»РёР· Р±СѓРєРІ" << endl << "----------------" << endl;
+	for(char c='Р°'; c<='СЏ'; c++){
+        cout << "Р‘СѓРєРІР° " << c << " " << letter[(int(c)+ANSI_OFFSET)] << endl;}
     
     cout << "----------------" << endl;
-	system("pause"); // системный вызов
-	
-    cout << endl << "Частотный анализ длины слов" << endl << "--------------------------" << endl;
-    for(i=1;i<34;i++){
-		cout << "Слов длиной с " << i << " буквами: " << l_word[i] << endl;}
-		    		    
-    cout << "--------------------------" << endl << "Проанализировано" << endl
-	     << "Букв: " << count_char << endl 
-		 << "Слов: " << count_word << endl << endl;
 	system("pause");
+	
+    cout << endl << "Р§Р°СЃС‚РѕС‚РЅС‹Р№ Р°РЅР°Р»РёР· РґР»РёРЅС‹ СЃР»РѕРІ" << endl << "--------------------------" << endl;
+    for(i=1;i<34;i++){
+	cout << "РЎР»РѕРІ РґР»РёРЅРѕР№ СЃ " << i << " Р±СѓРєРІР°РјРё: " << l_word[i] << endl;}
+		    		    
+    cout << "--------------------------" << endl << "РџСЂРѕР°РЅР°Р»РёР·РёСЂРѕРІР°РЅРѕ" << endl
+	 << "Р‘СѓРєРІ: " << count_char << endl 
+	 << "РЎР»РѕРІ: " << count_word << endl << endl;
+    system("pause");
 return 0;}
